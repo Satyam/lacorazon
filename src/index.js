1f1f2eb4ec379e81795498493bbfbdff6a3c3e82
Satyam <satyam@satyam.com.ar>
2018-11-18 18:45:03 +0100

    Added router, bootstrap, icons Changed favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInfinity,
+  faTrashAlt,
+  faCheck,
+  faEdit,
+  faExclamationTriangle,
+  faMinusCircle,
+  faPlusCircle,
+  faExclamationCircle
+} from "@fortawesome/free-solid-svg-icons";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+library.add(
+  faInfinity,
+  faTrashAlt,
+  faCheck,
+  faEdit,
+  faExclamationTriangle,
+  faMinusCircle,
+  faPlusCircle,
+  faExclamationCircle
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
