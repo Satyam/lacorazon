fbddfd5ab4dbb2e9e22e28974add662bc3e5b8b3
Satyam <satyam@satyam.com.ar>
2018-11-22 11:40:09 +0100

    Centered content with gutter for wider screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { Container, Row, Col } from "reactstrap";
 import { Navigation } from "./Navigation";
 import "./App.css";
 import Users from "./Users";
@@ -16,13 +16,19 @@ function App() {
         <div className="row">
           <main className="main-content col-lg-12 col-md-12 col-sm-12 p-0">
             <Navigation />
-            <ErrorBoundary>
-              <div className="main-content-container container-fluid px-4">
-                <Route path="/users" component={Users} />
-                <Route path="/user/:id?" component={User} />
-                <Route path="/bookstores" component={Bookstores} />
-              </div>
-            </ErrorBoundary>
+            <Container>
+              <Row>
+                <Col sm="12" md={{ size: 8, offset: 2 }}>
+                  <ErrorBoundary>
+                    <div className="main-content-container container-fluid px-4">
+                      <Route path="/users" component={Users} />
+                      <Route path="/user/:id?" component={User} />
+                      <Route path="/bookstores" component={Bookstores} />
+                    </div>
+                  </ErrorBoundary>
+                </Col>
+              </Row>
+            </Container>
           </main>
         </div>
       </div>
