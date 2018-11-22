aef933dfa409bb91e78fa71a1e1da8283963a21e
Satyam <satyam@satyam.com.ar>
2018-11-22 11:40:23 +0100

    Added Formik and first test form

--- src/User.js
+++ src/User.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { UserProvider, UserContext } from "./context/user";
 import { ButtonIconAdd, ButtonIconDelete } from "./Icons";
 import { Form, FormGroup, Label, Input } from "reactstrap";
+import { Formik, Form as KForm, Field as KField } from "formik";
 
 function UserForm({ id }) {
   const { user, error, addUser, deleteUser } = useContext(UserContext);
@@ -10,45 +11,50 @@ function UserForm({ id }) {
   }
   return (
     <>
-      <h1>Vendedores</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <Form>
-        <FormGroup>
-          <Label for="codigo">Código</Label>
-          <Input type="text" name="codigo" id="codigo" defaultValue={user.id} />
-        </FormGroup>
-        <FormGroup>
-          <Label for="codigo">Alias</Label>
-          <Input
-            type="text"
-            name="alias"
-            id="alias"
-            defaultValue={user.alias}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="codigo">Nombre</Label>
-          <Input
-            type="text"
-            name="nombre"
-            id="nombre"
-            defaultValue={user.name}
-          />
-        </FormGroup>
-        <ButtonIconAdd
-          className="mr-2"
-          onClick={() => {
-            console.log("accept");
-          }}
-          label="agregar"
-        />
-        <ButtonIconDelete
-          onClick={() => {
-            console.log("borrar");
-          }}
-          label="borrar"
-        />
-      </Form>
+      <h1>Add/Edit Vendedor</h1>
+      <Formik
+        initialValues={Object.assign({ id: "", name: "", alias: "" }, user)}
+        enableReinitialize={true}
+        isInitialValid={true}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting, isValid }) => (
+          <Form tag={KForm}>
+            <FormGroup>
+              <Label for="id">Código</Label>
+              <Input tag={KField} type="text" name="id" id="id" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="alias">Alias</Label>
+              <Input tag={KField} type="text" name="alias" id="alias" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="name">Nombre</Label>
+              <Input tag={KField} type="text" name="name" id="name" />
+            </FormGroup>
+            <ButtonIconAdd
+              type="submit"
+              disabled={isSubmitting || !isValid}
+              className="mr-2"
+              onClick={() => {
+                console.log("accept");
+              }}
+              label="Agregar"
+            />
+            <ButtonIconDelete
+              onClick={() => {
+                console.log("borrar");
+              }}
+              label="borrar"
+            />
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
