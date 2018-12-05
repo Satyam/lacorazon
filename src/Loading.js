a5f3c294abe0d93dd12ed2f35ce1bf6aaafc0d8b
Satyam <satyam@satyam.com.ar>
2018-12-05 18:19:42 +0100

    Added a nice Loading... modal

---
+++ src/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import icon from './loading.gif';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import styles from './Loading.module.css';
+
+export default function Loading({ title = '' }) {
+  return (
+    <Modal isOpen={true}>
+      <ModalHeader className={styles.header}>{title}</ModalHeader>
+      <ModalBody className={styles.container}>
+        <img src={icon} alt="loading..." />
+      </ModalBody>
+    </Modal>
+  );
+}
