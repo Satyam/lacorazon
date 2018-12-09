a995cfd339b1a39e09116d480f42beebe68b5e0f
Satyam <satyam@satyam.com.ar>
2018-12-09 15:27:34 +0100

    selector was missing dot separator

--- src/store/distribuidores/selectors.js
+++ src/store/distribuidores/selectors.js
@@ -1,5 +1,5 @@
 import { NAME } from './constants';
 export const selDistribuidores = NAME + '.data';
 export const selDistribuidoresIsLoading = NAME + '.isLoading';
-export const selDistribuidor = NAME + 'data.%0';
+export const selDistribuidor = NAME + '.data.%0';
 export const selDistribuidoresGotAll = NAME + '.gotAll';
