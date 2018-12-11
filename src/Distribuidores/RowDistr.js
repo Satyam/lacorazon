68b2e182a2e5e805f9713411ff27bf51e6175f0f
Satyam <satyam@satyam.com.ar>
2018-12-11 17:47:48 +0100

    Broke Distribuidores into separate files

---
+++ src/Distribuidores/RowDistr.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ButtonGroup } from 'reactstrap';
+import { ButtonIconEdit, ButtonIconDelete } from '../Icons';
+import styles from './styles.module.css';
+
+export default function RowDistr(id, data, history, deleteDistribuidor) {
+  return (
+    <tr key={id} onClick={() => history.push(`/distribuidor/${id}`)}>
+      <td>{data.nombre}</td>
+      <td>{data.contacto}</td>
+      <td>{data.telefono}</td>
+      <td className={styles.small}>{data.direccion}</td>
+      <td>{data.localidad}</td>
+      <td className={styles.small}>{(data.email || '').replace('@', '\n@')}</td>
+      <td>
+        <ButtonGroup size="sm">
+          <ButtonIconEdit
+            onClick={ev => {
+              ev.stopPropagation();
+              history.push(`/distribuidor/${id}?edit=true`);
+            }}
+          />
+          <ButtonIconDelete
+            onClick={ev => {
+              ev.stopPropagation();
+              deleteDistribuidor(id);
+            }}
+          />
+        </ButtonGroup>
+      </td>
+    </tr>
+  );
+}
---
+++ src/Distribuidores/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+import useReactRouter from 'use-react-router';
+import { ButtonIconAdd, ButtonSet } from '../Icons';
+
+import { useDispatch, useSelector } from '../store/hooks';
+import { isEmpty } from '../utils';
+import Loading from '../Loading';
+import Page from '../Page';
+import RowDistr from './RowDistr';
+import { acGetDistribuidores, acDeleteDistribuidor } from '../store/actions';
+
+import {
+  selDistribuidores,
+  selDistribuidoresIsLoading,
+  selDistribuidoresGotAll
+} from '../store/selectors';
+
+export default function Distribuidores() {
+  const [distribuidores, isLoading, gotAll] = useSelector([
+    selDistribuidores,
+    selDistribuidoresIsLoading,
+    selDistribuidoresGotAll
+  ]);
+  const [getDistrib, deleteDistribuidor] = useDispatch([
+    acGetDistribuidores,
+    acDeleteDistribuidor
+  ]);
+  if ((!isLoading && isEmpty(distribuidores)) || !gotAll) {
+    getDistrib();
+    return <Loading title="Distribuidores" />;
+  }
+  const { history } = useReactRouter();
+  return (
+    <Page wide title="Distribuidores" heading="Distribuidores">
+      <Table striped hover size="sm" responsive>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Contacto</th>
+            <th>Teléfono</th>
+            <th>Dirección</th>
+            <th>Localidad</th>
+            <th>e-Mail</th>
+
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(distribuidores).map(id =>
+            RowDistr(id, distribuidores[id], history, deleteDistribuidor)
+          )}
+        </tbody>
+      </Table>
+      <ButtonSet>
+        <ButtonIconAdd onClick={() => history.push('/distribuidor?edit=true')}>
+          Agregar
+        </ButtonIconAdd>
+      </ButtonSet>
+    </Page>
+  );
+}
