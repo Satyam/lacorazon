6ecb2ec53fb519df843b64f302fe9b1468773fe1
Satyam <satyam@satyam.com.ar>
2018-12-13 16:40:32 +0100

    cleared some testing code

--- src/Auth.js
+++ src/Auth.js
@@ -6,16 +6,12 @@ const auth = firebase.auth();
 auth.useDeviceLanguage();
 
 export function signIn() {
-  console.log('signIn current user', auth.currentUser);
-  debugger;
   if (!auth.currentUser) {
     auth.signInWithRedirect(provider);
   }
 }
 
 export function signOut() {
-  console.log('signOut current user', auth.currentUser);
-  debugger;
   if (auth.currentUser) {
     return auth.signOut();
   }
@@ -29,24 +25,15 @@ export function AuthProvider({ children }) {
   function userChanged(newUser) {
     return (newUser && newUser.uid) !== (user && user.uid);
   }
-  // auth.getRedirectResult().then(result => {
-  //   if (userChanged(result.user)) {
-  //     console.log('redirect', result.user);
-  //     setUser(result.user);
-  //   }
-  // });
-
   useEffect(
     () =>
       auth.onAuthStateChanged(newUser => {
         if (userChanged(newUser)) {
-          console.log('stateChange', newUser);
           setUser(newUser);
         }
       }),
     []
   );
-  console.log('provider', user);
   return (
     <UserContext.Provider value={user || ''}>{children}</UserContext.Provider>
   );
