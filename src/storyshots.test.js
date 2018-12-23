e0bfb9563024884c759cc5ceae3e3ec33692de51
Satyam <satyam@satyam.com.ar>
2018-12-23 18:30:39 +0100

    Fixed: FontAwesome had to be loaded for jest StoryBook loaded it in html head, but for background process, has to be loaded

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -3,6 +3,31 @@ import initStoryshots, { renderWithOptions } from '@storybook/addon-storyshots';
 import { mount } from 'enzyme';
 import { createSerializer } from 'enzyme-to-json';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faTrashAlt,
+  faCheckCircle,
+  faTimesCircle,
+  faEdit,
+  faExclamationTriangle,
+  faMinusCircle,
+  faPlusCircle,
+  faExclamationCircle,
+  faCalendarAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faTrashAlt,
+  faTimesCircle,
+  faCheckCircle,
+  faEdit,
+  faExclamationTriangle,
+  faMinusCircle,
+  faPlusCircle,
+  faExclamationCircle,
+  faCalendarAlt
+);
+
 initStoryshots({
   /* configuration options */
   test: renderWithOptions({
