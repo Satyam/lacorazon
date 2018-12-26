41338ad3c449c6b7b46d183a7c2f6188034aa4fe
Satyam <satyam@satyam.com.ar>
2018-12-26 16:26:09 +0100

    Improved formatting of LabeledText component Allows value to be provided either via the value property or as children Passing value in named property makes it compatible with other input components

--- src/Form/LabeledText.js
+++ src/Form/LabeledText.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { FormGroup, Label, Col } from 'reactstrap';
+import classNames from 'classnames/bind';
+import styles from './styles.module.css';
 
-export default function LabeledText({ label, value, pre }) {
+const cx = classNames.bind(styles);
+
+export default function LabeledText({ label, value, children, pre }) {
   return (
     <FormGroup row>
       <Label xs={12} lg={2}>
         {label}
       </Label>
-      <Col
-        xs={12}
-        lg={8}
-        style={{
-          whiteSpace: pre ? 'pre-line' : 'inherit',
-          border: 'thin solid silver',
-          borderRadius: '0.3em'
-        }}
-      >
-        {value}
+      <Col xs={12} lg={8}>
+        <div className={cx('form-control', { 'labeled-pre': pre })}>
+          {value}
+          {children}
+        </div>
       </Col>
     </FormGroup>
   );
