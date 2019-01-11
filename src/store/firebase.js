d7adffcce00b76c83e8ad61b9e88b07eae00380d
Satyam <satyam@satyam.com.ar>
2019-01-11 16:19:32 +0100

    Enabled the Service Worker for off-line caching. Same for Firestore

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -13,6 +13,18 @@ export const db = firebase.firestore();
 db.settings({
   timestampsInSnapshots: true
 });
+db.enablePersistence().catch(function(err) {
+  if (err.code === 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a a time.
+    // ...
+  } else if (err.code === 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+    // ...
+  }
+  console.error(err);
+});
 
 export const auth = firebase.auth();
 auth.useDeviceLanguage();
